2d438e124cc3f78302eb7ea65ab9960b5cbb3f29
sean <smbsporq@gmail.com>
2013-06-30 00:10:06 -0600

    Primary file and package definition

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-mytracks-knowledge
-==================
+My Tracks data processing program
 
-My Tracks parser and knowledge discovery
+This program is intended to parse and find interesting information about data 
+exported in CSV format from Google's My Tracks app. Maybe also some other things.
+
+This program incorporates the split-sequence library.
+
+Included Files:
+
+parse.lisp
+
+Known Issues:
+
+None at this point
---
+++ main.lisp
@@ -0,0 +1,15 @@
+;;;; This is the main file for running all of the My Tracks knowledge 
+;;;; finder functions and abilities
+
+;;;; Author: progdrum
+;;;; Authored: June 30, 2013
+
+(defpackage :mytracks-knowledge-finder
+  (:nickname :mkf)
+  (:use :common-lisp)
+  (:documentation "The primary package for parsing and processing data"))
+
+;; The main function to parse a data file and perform a data analysis. 
+;; Later on, there may be several versions of this for different analyses.
+(defun run-analysis (filename)
+  (parse:parse-and-store filename))

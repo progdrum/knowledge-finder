e6bdba85f9507c9dae5a889dbd8cbaa36ab3c6e9
sean <smbsporq@gmail.com>
2013-06-30 00:23:22 -0600

    A struct and a new idea

--- main.lisp
+++ main.lisp
@@ -9,6 +9,8 @@
   (:use :common-lisp)
   (:documentation "The primary package for parsing and processing data"))
 
+(in-package :mkf)
+
 ;; The main function to parse a data file and perform a data analysis. 
 ;; Later on, there may be several versions of this for different analyses.
 (defun run-analysis (filename)
--- parse.lisp
+++ parse.lisp
@@ -13,9 +13,27 @@
 
 (in-package :parse)
 
+;; Struct in which parsed data is to be stored
+;; Perhaps generalize by making this an object and making PARSE-AND-STORE 
+;; a generic function to dispatch on different objects for processing 
+;; different kinds of data sets?
+(defstruct mytracks-data
+  (segment 0 :type integer)
+  (point 0 :type integer)
+  (latitude 0.0 :type float)
+  (longitude 0.0 :type float)
+  (altitude 0.0 :type float)
+  (bearing 0.0 :type float)
+  (accuracy 0 :type integer)
+  (speed 0.0 :type float)
+  (time "" :type string)
+  (power nil)
+  (cadence nil)
+  (heart-rate nil))
+
 ;; Use split-sequence to break on commas and store (in struct?) 
 ;; for further processing
-(defun parse-and-store ()
-  (with-open-file (csv-input #|file-placeholder|# :direction :input)
+(defun parse-and-store (file)
+  (with-open-file (csv-input file :direction :input)
     ; The following line also needs to store the sequence.
     (split-sequence:split-sequence #\, #|Line read in|#)))
